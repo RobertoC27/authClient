852a39ebc403dfc8c5f8d30474c040a12c219bcf
RobertoC27 <chiroy_134@hotmail.com>
2018-12-13 23:17:52 +0000

    added validation and removed FORM and FIELD components

--- src/AdvancedForm.js
+++ src/AdvancedForm.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik} from 'formik';
+import * as Yup from 'yup';
+
 
 const advancedForm = (props) => {
     return (
         <Formik
-            
+            initialValues={{ email: props.email, password: props.password, endpoint: props.endpoint }}
             onSubmit={async (values, actions) => {
                 const success = await props.customSubmit(values);
                 actions.setSubmitting(false);
-                
-                // Esto por alguna razon da un error 
-                // if (success)
-                //     actions.resetForm();
+                if (success)
+                    actions.resetForm();
             }}
+            validationSchema={Yup.object().shape({
+                email: Yup.string().email().required(),
+                password: Yup.string().min(5).max(40).required()
+            })}
             render={props => (
-                <Form>
-                    <Field type="email" name="email" placeholder="Email" />
-                    <Field type="password" name="password" placeholder="Password" />
+                <form onSubmit={props.handleSubmit}>
+                    {props.errors.email && props.touched.email && <div>{props.errors.email}</div>}
+                    <input type="email" name="email" placeholder="Email" value={props.values.email} onChange={props.handleChange} onBlur={props.handleBlur} />
+                    {props.errors.password && props.touched.password && <div>{props.errors.password}</div>}
+                    <input type="password" name="password" placeholder="Password" value={props.values.password} onChange={props.handleChange} onBlur={props.handleBlur} />
                     <button type="submit">Submit</button>
-                </Form>
+                </form>
             )}
         />
     )
 }
-
+// A component is changing an uncontrolled input of type email to be controlled
+// A component is changing an uncontrolledd
 export default advancedForm;
